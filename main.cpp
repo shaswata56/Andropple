86c5cc0d523e2d616c7db516640ff62ea7c50f8a
Shaswata Das <shaswata56@gmail.com>
2018-08-25 15:15:38 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include "load.h"
 #include "game.h"
 
-
-
 int main()
 {
+    int z=0;
     int x=start_game();
     if(x==1)
     {
         int y = play();
-        if(y) main();
+        if(y>0) z=Game_Over(y);
+        else main();
+        if(z) main();
     }
     if(x==2)
     {
         int y = show_obj();
+        if(y) main();
+    }
+    if(x==3)
+    {
+        int y = highscore();
+        if(y) main();
+    }
+    if(x==4)
+    {
+        int y = About();
         if(y) main();
     }
     return 0;
